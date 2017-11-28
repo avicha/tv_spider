2858e5f19a8f242357c2bf2c22d56cd69494ebd2
AV <av@admindeMacBook-Pro.local>
2017-11-28 11:19:57 +0800

    feat:add the mongodb config

---
+++ tv_spider/mongodb_config.example.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+host = '127.0.0.1'
+port = '27017'
+tz_aware = False
+connect = True
+options = {
+    'username': '',
+    'password': '',
+    'authSource': 'smart_tv'
+}
--- tv_spider/spiders/store.py
+++ tv_spider/spiders/store.py
@@ -1,3 +1,4 @@
 import pymongo
-client = pymongo.MongoClient(host='localhost', port=27017, tz_aware=False, connect=True)
+import tv_spider.mongodb_config as db_config
+client = pymongo.MongoClient(host=db_config.host, port=db_config.port, tz_aware=db_config.tz_aware, connect=db_config.connect, **db_config.options)
 db = client.smart_tv
